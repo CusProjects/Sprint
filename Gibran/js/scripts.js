62a9a41bfa9dedb613ef0f273f07b587c3b6bb98
Jose Gibran <jpolonsky2013382@kinal.edu.gt>
2015-10-28 18:31:30 -0600

    Mostrar proyect y Tasks

--- Gibran/js/scripts.js
+++ Gibran/js/scripts.js
@@ -3,8 +3,30 @@ var app = angular.module('Proyect', []);
 
 
 app.controller('ProyectsController', function($scope, $http) {
-
-    $http.get("http://c-us.xyz/Sprint/public/api/difficulties")
-        .success(function(response) {$scope.names = response;});
-
+    $http.get("http://c-us.xyz/Sprint/public/api/proyects/"+getUrlParameter('id'))
+        .success(function(response) {
+         $scope.proyect = response;
+        }
+    );
+    $http.get("http://c-us.xyz/Sprint/public/api/tasks")
+        .success(function(response) {
+            $scope.tasks = response;
+        }
+    );
 });
+
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
