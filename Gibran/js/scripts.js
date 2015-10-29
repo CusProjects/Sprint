9ce2eeb0c56cfa1f671d7553ef3750443df8b425
C-us Developers <lcolindres-2013081@kinal.edu.gt>
2015-10-28 22:36:47 -0600

    Update scripts.js

--- Gibran/js/scripts.js
+++ Gibran/js/scripts.js
@@ -2,17 +2,41 @@ var app = angular.module('Proyect', []);
 
 
 
-app.controller('ProyectsController', function($scope, $http) {
-    $http.get("http://c-us.xyz/Sprint/public/api/proyects/"+getUrlParameter('id'))
+app.controller('ProyectsController', function($scope, $http){
+    $scope.proyect = {};
+    $scope.tasks = [];
+    
+    /*$http.get("http://c-us.xyz/Sprint/public/api/proyects/"+getUrlParameter('id'))
         .success(function(response) {
          $scope.proyect = response;
         }
-    );
-    $http.get("http://c-us.xyz/Sprint/public/api/tasks")
+    );*/
+    ajaxHelper("http://c-us.xyz/Sprint/public/api/proyects/"+getUrlParameter('id'), "GET")
+        .success(function(data){
+            $scope.proyect = data;
+        });
+    
+    /*$http.get("http://c-us.xyz/Sprint/public/api/tasks")
         .success(function(response) {
             $scope.tasks = response;
         }
-    );
+    );*/
+    ajaxHelper("http://c-us.xyz/Sprint/public/api/tasks", "GET").success(function(data){
+       $scope.tasks = data; 
+    });
+    
+    function ajaxHelper(uri, method, data){
+		return $.ajax({
+			type: method,
+			async: false,
+			url: uri,
+			datatype: 'json',
+			contentType: 'application/json',
+			data: data ? JSON.stringify(data) : null
+		}).fail(function(err){
+			console.log(err);
+		});
+	}
 });
 
 
