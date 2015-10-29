c975f1cad789600c4d4c74802e78b18efe97c1a0
JGibran <jpolonsky2013382@kinal.edu.gt>
2015-10-29 07:41:13 -0600

    Vista de task por Stack arreglado

--- Gibran/js/scripts.js
+++ Gibran/js/scripts.js
@@ -2,41 +2,24 @@ var app = angular.module('Proyect', []);
 
 
 
-app.controller('ProyectsController', function($scope, $http){
+app.controller('ProyectsController', function($scope){
     $scope.proyect = {};
+    $scope.stacks = [];
     $scope.tasks = [];
-    
-    /*$http.get("http://c-us.xyz/Sprint/public/api/proyects/"+getUrlParameter('id'))
-        .success(function(response) {
-         $scope.proyect = response;
-        }
-    );*/
     ajaxHelper("http://c-us.xyz/Sprint/public/api/proyects/"+getUrlParameter('id'), "GET")
         .success(function(data){
             $scope.proyect = data;
         });
-    
-    /*$http.get("http://c-us.xyz/Sprint/public/api/tasks")
-        .success(function(response) {
-            $scope.tasks = response;
-        }
-    );*/
-    ajaxHelper("http://c-us.xyz/Sprint/public/api/tasks", "GET").success(function(data){
-       $scope.tasks = data; 
+    ajaxHelper("http://c-us.xyz/Sprint/public/api/stacks/proyect/" + $scope.proyect.id, "GET").success(function(data){
+       $scope.stacks = data;
     });
-    
-    function ajaxHelper(uri, method, data){
-		return $.ajax({
-			type: method,
-			async: false,
-			url: uri,
-			datatype: 'json',
-			contentType: 'application/json',
-			data: data ? JSON.stringify(data) : null
-		}).fail(function(err){
-			console.log(err);
-		});
-	}
+
+    $scope.setTasks = function(id) {
+
+        ajaxHelper("http://c-us.xyz/Sprint/public/api/tasks/stack/" + id, "GET").success(function(data){
+            $scope.tasks = data;
+        });
+    };
 });
 
 
@@ -54,3 +37,16 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
+
+function ajaxHelper(uri, method, data){
+    return $.ajax({
+        type: method,
+        async: false,
+        url: uri,
+        datatype: 'json',
+        contentType: 'application/json',
+        data: data ? JSON.stringify(data) : null
+    }).fail(function(err){
+        console.log(err);
+    });
+}
